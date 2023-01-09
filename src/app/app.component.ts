36b22bc44917e0e8a5cbcde211246e0927d46c01
Romashka <rroma13579@gmail.com>
2023-01-09 21:09:50 +0100

    user data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserData } from './app';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'Angular_3';
+
   status1 = 'User';
   status2 = 'Roma';
   statusAuto = 'none';
   statusPicked = this.statusAuto;
-  Choose1() {this.statusPicked = this.status1;}
-  Choose2() {this.statusPicked = this.status2;}
-  Choose3() {this.statusPicked = this.statusAuto;}
+
+  Choose1(a:number) { 
+    if(a == 1){this.statusPicked = this.status1;}
+    else if(a == 2){this.statusPicked = this.status2;}
+    else if(a == 3){this.statusPicked = this.statusAuto;}
+  }
+
+  userList: UserData[] = [];
+  
+  ngOnInit(): void{
+    this.userList = [
+      {    
+        name: 'Roman',
+        surname: 'Rudenko',
+        age: 16,
+        hobbies: 'programming and playing the guitar',
+        born: new Date('03-26-2006')
+      },
+      {    
+        name: 'User',
+        surname: 'none',
+        age: 'unknown',
+        hobbies: 'unknown',
+        born: new Date('00-00-0000')
+      }
+    ]
+  }
+
 }
---
+++ src/app/app.ts
@@ -0,0 +1,7 @@
+export interface UserData{
+    name: string,
+    surname: string,
+    age: any,
+    hobbies: string,
+    born: Date,
+}
