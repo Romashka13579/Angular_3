cb59c23d78e2d26c0547696f3098d7107ade70f5
Romashka <rroma13579@gmail.com>
2023-01-09 20:49:05 +0100

    status change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular_3';
+  status1 = 'User';
+  status2 = 'Roma';
+  statusAuto = 'none';
+  statusPicked = this.statusAuto;
+  Choose1() {this.statusPicked = this.status1;}
+  Choose2() {this.statusPicked = this.status2;}
+  Choose3() {this.statusPicked = this.statusAuto;}
 }
---
+++ src/app/roma/roma.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'status-roma',
+  templateUrl: './roma.component.html',
+  styleUrls: ['./roma.component.css']
+})
+export class RomaComponent {
+
+}
